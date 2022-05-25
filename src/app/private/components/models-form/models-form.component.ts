45a0c017c5082eab8c0637721182c5e9eae97a45
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-05-24 21:56:57 -0300

    ligando o modelo a coleção

--- src/app/private/components/models-form/models-form.component.ts
+++ src/app/private/components/models-form/models-form.component.ts
@@ -5,6 +5,8 @@ import { ModelsService } from '../../services/models.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { CollectionService } from '../../services/collection.service';
+import { CollectionInterface } from '../../interfaces/collection-interface';
 
 @Component({
   selector: 'app-models-form',
@@ -15,15 +17,71 @@ export class ModelsFormComponent implements OnInit {
   public form!: FormGroup;
   public submitted = false;
   public model$!: Observable<ModelsInterface[]>;
+  public collectionList$!: Observable<CollectionInterface[]>;
+  public modelTypes = [
+    'Bermuda',
+    'Biquini',
+    'Bolsa',
+    'Boné',
+    'Calça',
+    'Calçados',
+    'Camisa',
+    'Chapéu',
+    'Saia',
+  ];
 
   constructor(
     private _fb: FormBuilder,
+    private _collectionService: CollectionService,
     private _modelService: ModelsService,
     private _route: ActivatedRoute,
     private _location: Location
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formGroup();
+    this.collectionList$ = this._collectionService.list();
+  }
+
+  public formGroup() {
+    return (this.form = this._fb.group({
+      id: null,
+      nome: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      responsavel: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      tipo: [
+        'Tipo do Modelo',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      colecao: [
+        'Selecionar Coleção',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      bordado: false,
+      estampa: false,
+    }));
+  }
 
   public onSubmit() {
     if (this.form.valid) {
@@ -33,12 +91,12 @@ export class ModelsFormComponent implements OnInit {
         this._location.back();
       } else {
         this._modelService.create(this.form.value).subscribe();
+        console.log(this.form.value);
         this._location.back();
       }
     }
   }
   public onCancel() {
-    this.submitted = false;
     this.form.reset();
     this._location.back();
   }
