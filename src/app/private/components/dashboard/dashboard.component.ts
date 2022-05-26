394e4c68489f85bac6cf4b80791cdcdd8a6009a0
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-05-26 20:23:21 -0300

    Estruturando a dashboard

--- src/app/private/components/dashboard/dashboard.component.ts
+++ src/app/private/components/dashboard/dashboard.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { CollectionInterface } from '../../interfaces/collection-interface';
+import { CollectionService } from '../../services/collection.service';
+import { ModelsService } from '../../services/models.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public collection!: CollectionInterface[];
+  public average = 0;
+  public total = 0;
+  public totalCollections = 0;
+  public totalModels = 0;
 
-  constructor() { }
+  constructor(
+    private _collectionService: CollectionService,
+    private _modelService: ModelsService
+  ) {}
 
   ngOnInit(): void {
+    this.orderedList();
+    this.averageBudget();
+    this.countCollections();
+    this.countModels();
   }
-
+  orderedList() {
+    this._collectionService.list().subscribe((array) => {
+      array.sort((a, b) => b.orcamento - a.orcamento);
+      this.collection = array;
+    });
+  }
+  countCollections() {
+    this._collectionService.list().subscribe((array) => {
+      for (let i = 0; i < array.length; i++) {
+        this.totalCollections++;
+      }
+    });
+  }
+  countModels() {
+    this._modelService.list().subscribe((array) => {
+      for (let i = 0; i < array.length; i++) {
+        this.totalModels++;
+      }
+    });
+  }
+  averageBudget() {
+    this._collectionService.list().subscribe((array) => {
+      array.forEach((budget) => {
+        return (this.total += budget.orcamento);
+      });
+      return (this.average = this.total / array.length);
+    });
+  }
 }
