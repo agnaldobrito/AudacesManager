9f0e64ed6f3594fc1ab165d2584b36b26eb26875
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-05-29 07:25:01 -0300

    modificando a pipe currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
 import { PrivateModule } from './private/private.module';
 import { PublicModule } from './public/public.module';
+registerLocaleData(localePt);
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -18,7 +22,12 @@ import { PublicModule } from './public/public.module';
     PrivateModule,
     PublicModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
