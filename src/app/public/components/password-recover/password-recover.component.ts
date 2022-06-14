1a5961ba5f04a3a54c1a90ebf1e018c426ff1b14
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-14 09:00:40 -0300

    fix: validação do form

--- src/app/public/components/password-recover/password-recover.component.ts
+++ src/app/public/components/password-recover/password-recover.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-password-recover',
   templateUrl: './password-recover.component.html',
-  styleUrls: ['./password-recover.component.scss']
+  styleUrls: ['./password-recover.component.scss'],
 })
 export class PasswordRecoverComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder, private _router: Router) {}
 
   ngOnInit(): void {
+    this.form = this._fb.group({
+      email: [null, [Validators.required, Validators.email]],
+    });
   }
-
+  onSubmit() {
+    if (this.form.valid) {
+      this._router.navigate(['/login']);
+    }
+    console.log(this.form.valid);
+  }
 }
